68d1f11e0736ff774abdf8d997b00a1b574b2c36
Marcos Sardina <marcos.sardina@gmail.com>
2024-04-14 19:57:13 +1000

    all buttons done

--- main.py
+++ main.py
@@ -21,17 +21,30 @@ def scale_img(img, scale):
     return pygame.transform.scale(
         img, (img.get_width() * scale, img.get_height() * scale)
     )
-
-
+def blit_centre(img, centerx, centery, SCREEN):
+    newx = centerx - img.get_width() / 2
+    newy = centery - img.get_height() / 2
+    newpos = (newx, newy)
+    SCREEN.blit(img, (newpos))
+    
+def center2topright(img, centerx, centery, SCREEN):
+    newx = centerx - img.get_width() / 2
+    newy = centery - img.get_height() / 2
+    newpos = (newx, newy)
+    return newpos
+    
 # load images
 backdrop_img = pygame.image.load(os.path.join("assets", "backgrounds", "backdrop.svg"))
 title_img = pygame.image.load(os.path.join("assets", "buttons", "title.png"))
-play_img = pygame.image.load(os.path.join("assets", "buttons", "play.svg"))
+play_img = pygame.image.load(os.path.join("assets", "buttons", "play.png"))
 arrow_img = pygame.image.load(os.path.join("assets", "buttons", "arrow.png"))
 
-title_img = scale_img(title_img, 0.5)
-arrow_img = scale_img(arrow_img, 0.12)
-
+title_normal_img = scale_img(title_img, 0.5)
+title_zoom_img = scale_img(title_img, 0.56)
+arrow_normal_img = scale_img(arrow_img, 0.12)
+arrow_zoom_img = scale_img(arrow_img, 0.14)
+play_normal_img = scale_img(play_img, 0.21)
+play_zoom_img = scale_img(play_img, 0.25)
 
 # screen sizing
 SCALE = 2
@@ -46,6 +59,45 @@ pygame.display.set_caption("HOT CHOCOLATE v0.1 || A WARM AND FUZZY GAME")
 # lets re-load backdrop image scaled to screen
 backdrop_img = stretch_to_screen(backdrop_img, SCREEN)
 
+
+#class
+
+class Button:
+    def __init__(self, x, y, img, img_zoom):
+        self.pos = (x, y)
+        self.x = self.pos[0]
+        self.y = self.pos[1]
+        self.img = img
+        self.imgzoom = img_zoom
+        self.useimg = self.img
+        
+        
+        self.rect = pygame.rect.Rect(self.x - self.img.get_width() / 2, self.y - self.img.get_height() / 2, self.img.get_width(), self.img.get_height())
+        self.hover = False
+        
+    def draw(self):
+        if self.hover:
+            blit_centre(self.imgzoom, self.x, self.y, SCREEN)
+        else:
+            blit_centre(self.img, self.x, self.y, SCREEN)
+        
+        # pygame.draw.rect(SCREEN, (0, 0, 0), self.rect)
+        
+    def update(self):
+        pos = pygame.mouse.get_pos()
+        
+        self.hover = False
+        if self.rect.collidepoint(pos):
+            self.hover = True
+
+
+    
+#objects
+play_btn = Button(WIDTH / 2, HEIGHT / 2, play_normal_img, play_zoom_img)
+title_btn = Button(WIDTH / 2, 0 + title_normal_img.get_height() / 2, title_normal_img, title_zoom_img)
+arrow_btn = Button(WIDTH / 2 - 150, HEIGHT / 2 + 200 , arrow_normal_img, arrow_zoom_img)
+
+buttons = [title_btn, play_btn, arrow_btn]
 
 def title():
     run = True
@@ -62,25 +114,12 @@ def title():
         # draw screen
 
         SCREEN.blit(backdrop_img, (0, 0))
-        SCREEN.blit(title_img, (WIDTH / 2 - title_img.get_width() / 2, 0))
-        SCREEN.blit(
-            play_img,
-            (
-                WIDTH / 2 - play_img.get_width() / 2,
-                HEIGHT / 2 - play_img.get_height() / 2,
-            ),
-        )
 
-        SCREEN.blit(
-            arrow_img,
-            (
-                WIDTH / 2 - arrow_img.get_width() / 2 - 100,
-                HEIGHT / 2 - arrow_img.get_height() / 2 + 200,
-            ),
-        )
+        for btn in buttons:
+            btn.update()
+            btn.draw()
 
-        # check if play button pressed
-
+        
         # update
         pygame.display.update()
 
