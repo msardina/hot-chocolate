391d72162b5c0d16526b6745c63ffc76e04037cf
Marcos Sardina <marcos.sardina@gmail.com>
2024-04-14 17:18:51 +1000

    initial screen fix svg bug Co-authored-by: Sebastian Sardina <ssardina@gmail.com>

--- main.py
+++ main.py
@@ -7,36 +7,34 @@ from pygame import mixer
 pygame.init()
 mixer.init()
 
-# imgs
+# Load an image and scale it to fit the screen
+def load_image(img, SCREEN):
+    size_img = img.get_size()
+    scale = min(
+        SCREEN.get_width() / size_img[0], SCREEN.get_height() / size_img[1]
+    )
+    return pygame.transform.scale(
+        img, (round(size_img[0] * scale), round(size_img[1] * scale))
+    )
+
+# load images
+backdrop_img = pygame.image.load(os.path.join("assets", "backgrounds", "backdrop.svg"))
 title_img = pygame.image.load(os.path.join("assets", "buttons", "title.svg"))
 play_img = pygame.image.load(os.path.join("assets", "buttons", "play.svg"))
 arrow_img = pygame.image.load(os.path.join("assets", "buttons", "arrow.svg"))
 
+# screen sizing
 SCALE = 2
-
-# lets build the backdrop image
-backdrop = pygame.image.load(os.path.join("assets", "backgrounds", "backdrop.svg"))
-
-
-WIDTH = backdrop.get_width() * SCALE
-HEIGHT = backdrop.get_height() * SCALE
+WIDTH = backdrop_img.get_width() * SCALE
+HEIGHT = backdrop_img.get_height() * SCALE
 
 # screen setup
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("HOT CHOCOLATE v0.1 || A WARM AND FUZZY GAME")
 
-# scale backdrop to stretch to SCREEN
-size_backdrop = backdrop.get_size()
-scale = min(
-    SCREEN.get_width() / size_backdrop[0], SCREEN.get_height() / size_backdrop[1]
-)
-backdrop = pygame.transform.scale(
-    backdrop, (round(size_backdrop[0] * scale), round(size_backdrop[1] * scale))
-)
 
-# setup font
-
-# functions
+# lets re-load backdrop image scaled to screen
+backdrop_img = load_image(backdrop_img, SCREEN)
 
 
 def title():
@@ -50,10 +48,10 @@ def title():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        # draw
+                
+        # draw screen
 
-        # SCREEN.fill((255, 255, 255))
-        SCREEN.blit(backdrop, (0, 0))
+        SCREEN.blit(backdrop_img, (0, 0))
         SCREEN.blit(title_img, (WIDTH / 2 - title_img.get_width() / 2, 0))
         SCREEN.blit(
             play_img,
@@ -71,7 +69,8 @@ def title():
             ),
         )
 
-        # check if play
+        # check if play button pressed
+
 
         # update
         pygame.display.update()
